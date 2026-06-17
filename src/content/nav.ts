a1570ce2be2ae9e1fa688a677e2e5ae4246275e7
Kevin Kiragu <kevomagunas439@gmail.com>
2026-06-17 14:42:10 +0300

    chore: remove obsolete nav links

--- src/content/nav.ts
+++ src/content/nav.ts
@@ -3,9 +3,6 @@ export const navConfig = {
     { title: "Home", href: "/" },
     { title: "About", href: "/about" },
     { title: "Services", href: "/services" },
-    { title: "Policies", href: "/policies" },
-    { title: "Claims", href: "/claims" },
-    { title: "Insights", href: "/insights" },
     { title: "Contact", href: "/contact" },
   ],
 };
